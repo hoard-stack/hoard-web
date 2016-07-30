03aab1fe72f3fb519090789ab3e1b4935fdb8ee1
Piotr Gankiewicz <spetz90@gmail.com>
2016-07-30 19:28:52 +0200

    Basic setup of local auth using passport.js #3

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+var databaseConfig = require('./config/database.js');
+require('./config/passport')(passport);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+mongoose.connect(databaseConfig.connectionString);
 
 var app = express();
 
@@ -17,13 +22,19 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// required for passport
+app.use(session({ secret: 'secret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.use('/', routes);
-app.use('/users', users);
+// routes ======================================================================
+require('./routes.js')(app, passport);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
